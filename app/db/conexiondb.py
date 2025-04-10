98e189550aadd66eea11f439f2cdfde0de65f63a
Diesanz <diego.sanzr2003@gmail.com>
2025-04-10 12:33:18 +0200

    docs(db): Mejora documentación y manejo de errores en funciones de base de datos

--- app/db/conexiondb.py
+++ app/db/conexiondb.py
@@ -24,6 +24,17 @@ def get_connection():
 
 
 def select_db(query, args=(), one=False):
+    """Función destinada a realizar consultas SELECT en la base de datos.
+
+    Keyword arguments:
+    query -- consulta SQL que se ejecuta en la base de datos.
+    args -- parámetros que se deben sustituir en la consulta SQL (por defecto es una tupla vacía).
+    one -- si es True, devuelve solo un resultado (el primer elemento) en lugar de una lista de resultados.
+
+    Return:
+    Si 'one' es True, devuelve un solo valor. Si 'one' es False, devuelve una lista de resultados.
+    """
+    
     connection = None
 
     try:
@@ -33,19 +44,29 @@ def select_db(query, args=(), one=False):
             cursor.execute(query, args)
             result = cursor.fetchall()
 
-            if one:
+            if one:  # En caso de que solo se devuelva un solo resultado (por ejemplo, un usuario o un producto)
                 return result[0]
             else:
-                return result
+                return result  # Devuelve una lista de resultados
 
     except Exception as e:
         print(f"Error al ejecutar la consulta: {e}")
     finally:
         if connection: 
-            #si la conexión es válida se cierra conexión
+            # Si la conexión es válida, se cierra la conexión
             connection.close()
 
-def execute_db(query, args=(), one=False):
+
+def execute_db(query, args=()):
+    """Función destinada a ejecutar consultas que modifican la base de datos (INSERT, UPDATE, DELETE).
+
+    Keyword arguments:
+    query -- consulta SQL que se ejecuta en la base de datos.
+    args -- parámetros que se deben sustituir en la consulta SQL (por defecto es una tupla vacía).
+
+    Return:
+    No devuelve nada, pero se realiza un commit en la base de datos para aplicar cambios.
+    """
     connection = None
 
     try:
@@ -53,10 +74,10 @@ def execute_db(query, args=(), one=False):
 
         with connection.cursor() as cursor:
             cursor.execute(query, args)
-            connection.commit()
+            connection.commit()  # Realiza el commit para asegurar que los cambios se guarden
     except Exception as e:
         print(f"Error al ejecutar la consulta: {e}")
     finally:
         if connection: 
-            #si la conexión es válida se cierra conexión
+            # Si la conexión es válida, se cierra la conexión
             connection.close()
