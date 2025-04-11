a11d70590ef07c6fe5bf4c30cbf644fbef96a09a
Diesanz <diego.sanzr2003@gmail.com>
2025-04-11 16:21:20 +0200

    refactor(db): Mejora en la función de listado de usuarios y manejo de la conexión a la base de datos
    Se modificó la función `user_list` para retornar un string formateado con la lista de usuarios.  Además, se actualizaron las funciones `update_user` y `delete_user` para aceptar la conexión a la base de datos como parámetro y se ajustó la llamada a estas funciones en la sección `if __name__ == '__main__'`. Se corrigió el nombre de la tabla de 'users' a 'Usuario'.

--- app/db/como_utilizar_conexion_db.py
+++ app/db/como_utilizar_conexion_db.py
@@ -3,7 +3,7 @@ from conexiondb import get_connection, close_connection, select_db, execute_db
 def user_list(conn=None):
     query_db = "SELECT id, nombre FROM Usuario"
     users = select_db(query_db, conn=conn)
-    return users
+    return f'Usuarios: {users}'
 
 def add_user(conn=None):
     nombre = "Juan"
@@ -16,21 +16,22 @@ def add_user(conn=None):
     return f"✅ Usuario '{nombre}' agregado exitosamente!"
 
 
-def update_user(id, new_name):
-    query = "UPDATE users SET name = %s WHERE id = %s"
-    execute_db(query, (new_name, id))
+def update_user(id, new_name, conn):
+    query = "UPDATE Usuario SET nombre = %s WHERE id = %s"
+    execute_db(query, (new_name, id), conn=conn)
     return f"El usuario con ID {id} ha sido actualizado con el nuevo nombre {new_name}"
 
-def delete_user(id):
-    query = "DELETE FROM users WHERE id = %s"
-    execute_db(query, (id,))
+def delete_user(id, conn):
+    query = "DELETE FROM Usuario WHERE id = %s"
+    execute_db(query, (id,), conn=conn)
     return f"El usuario con ID {id} ha sido eliminado."
 
 if __name__ == '__main__':
     conn = get_connection()
     if conn:
-        users=user_list(conn)
-        print(f'Usuarios {users}')
-        add_user(conn)
+        print(user_list(conn))
+        print(add_user(conn))
+        print(update_user(1,"Diego",conn))
+        print(delete_user(6,conn))
 
         close_connection(conn)
