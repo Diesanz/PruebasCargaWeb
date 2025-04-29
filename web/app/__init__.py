9b849ae15aa1f6509750c18fb0997d77cd5948bb
victorfer69 <victor.fernandez0609@gmail.com>
2025-04-29 15:29:01 +0200

    Muestreo de los platos

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -8,9 +8,11 @@ def create_app():
     from .routes.autentificar import autentificar_usuarios
     from .routes.carritoController import carrito
     from .routes.menuController import menu
+    from .routes.productosController import producto
 
     app.register_blueprint(autentificar_usuarios)
     app.register_blueprint(carrito)
     app.register_blueprint(menu)
+    app.register_blueprint(producto)
 
     return app
---
+++ web/app/routes/productosController.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify, Blueprint, render_template, redirect, url_for, flash, make_response
+from app.db.conexiondb import Conexion
+from app.models.Producto import Producto
+from app.schemas.Producto import producto_schema
+
+producto = Blueprint('producto', __name__, url_prefix="/api")
+
+def get_produtosdb():
+    conn = Conexion()
+    query = "SELECT * FROM Producto" 
+    productos = conn.select_db(query)
+    
+    productos_obj = [Producto(**producto_schema(p)) for p in productos] #validamos datos y tipamos
+    
+    return productos
+
+@producto.route('/productos', methods=['GET'])
+def api_index():
+    return render_template("platos.html", productos = get_produtosdb()) # Renderiza el template para /api
